37a429c12783138243cd5661bff0919cf664d5b1
iam7kei <iam7kei@gmail.com>
2025-01-30 16:40:14 +0800

    updated db tables / update get and post routes for bank

--- src/api/bank.js
+++ src/api/bank.js
@@ -1,14 +1,34 @@
 import express from "express";
+import knexClient from "../utils/knex.js";
 
 const bankRoutes = express.Router();
 
-bankRoutes.get("/list", (req, res) => {
-  res.status(200).send("success");
+bankRoutes.get("/list", async (req, res) => {
+  try {
+    const result = await knexClient.select("*").from("banks")
+    res.status(200).send(result)
+  } catch (e) {
+    console.error(e)
+  }
 });
 
-bankRoutes.post("/add", (req, res) => {
-  
-  res.status(200).send("success");
+bankRoutes.post("/add", async (req, res) => {
+  const validTypes = ["SAVINGS", "CHECKING"]
+  try {
+    const { name, type, amount } = req.body
+   if (!validTypes.includes(type)) {
+     throw new Error(`Invalid type: ${type}.`);
+   }
+    const result = await knexClient('banks').insert({ name, type, amount: parseFloat(amount) }).returning('*')
+
+    res.status(200).send(result);
+  } catch (e) {
+    console.error(e)
+    
+   res.status(400).send({
+     message: e.message,
+   });
+  }
 });
 
 export default bankRoutes;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
+import "dotenv/config";
 import routes from "./api/routes.js"
 import express from "express"
-import db from "./utils/db.js";
-import "dotenv/config";
-import knexClient from "./utils/knex.js";
+import bodyParser from "body-parser";
 import {getTimeNow } from "./services/db.js";
 const app = express();
 const port = 3000;
 
 getTimeNow();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use("/", routes);
 
 app.listen(port, () => {
